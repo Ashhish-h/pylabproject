41b08577c2dae5bd44714c39209fd0bbd8bef218
Ashish <112895127+Ashhish-h@users.noreply.github.com>
2023-04-29 17:10:20 +0530

    GuessTheNumberGame.py
    This is a basic game in python in which user guesses a number if the number matches to the random number taken by computer user wins.

---
+++ GuessTheNumberGame.py
@@ -0,0 +1,26 @@
+Guess the number:
+In this game, the computer will guess a random number and the player will try to guess what
+number it is. The game ends when the player manages to guess the number. Display the appropriate
+score also.
+
+Code-
+import random
+number=random.randint(1,100) 
+name=input("Hi! What is your name\n")
+print('Okay '+name+ " Lets Start!")
+Count=0 #count number of guesses
+while(number):
+       guess=int(input("Guess the number between 1 and 100!\n"))
+       if (guess<number):
+              count+=1
+              print("Too low!")
+
+       elif(guess>number):
+              count+=1
+              print("Too high!")
+    
+       elif (guess==number):
+             count+=1
+             break
+if(guess==number):
+       print("You guessed the number in "+str(count)+ " trials!")
